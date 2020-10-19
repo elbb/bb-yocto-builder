c1acc72c1754642812f2e2c256558eb84ff86485
Marcel Lilienthal <134974+mlilien@users.noreply.github.com>
2020-10-19 09:56:59 +0200

    added default.env, local.env.template + doku how to use it; dobi.sh downloads dobi if not available; dobi.sh parameter checking; get rid of aliases; email notification in ci/cd on failure/success

--- dobi.sh
+++ dobi.sh
@@ -3,17 +3,58 @@
 set -o errexit   # abort on nonzero exitstatus
 set -o pipefail  # don't hide errors within pipes
 
-dobi --filename meta.yaml version # generate actual version files
+# load and export env files
+set -o allexport
+source default.env
+[[ -f local.env ]] && source local.env
+set +o allexport
 
-# check argument count
-if [ -z ${1} ]; then
-    echo "no arguments!"
-    dobi --filename meta.yaml list
+#check for docker
+if [[ $(docker version &>/dev/null;echo $?) -ne 0 ]]; then
+    echo "docker is not running"
     exit 1
 fi
 
-# load generated version infos
-source gen/gitversion/env/gitversion.env
+#check for dobi
+dobi=".dobi/dobi"
+if [[ ! -f ${dobi} ]]; then
+    if [[ -x $(which dobi) ]]; then
+        dobi=$(which dobi)
+    else
+        if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "cygwin" ]]; then
+            dobi_dltarget=linux
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+            dobi_dltarget=darwin
+        elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+            dobi_dltarget=windows
+        fi
+        if [[ ! -z ${dobi-dltarget} && -x $(which curl) ]]; then
+            mkdir -p .dobi
+            curl -L -o ${dobi} "https://github.com/dnephin/dobi/releases/download/v${DOWNLOAD_VERSION_DOBI}/dobi-${dobi_dltarget}"
+            if [[ ${?} -ne 0 ]]; then
+                echo "Download of dobi failed."
+                exit 1
+            else
+                chmod +x ${dobi}
+            fi
+        else
+            echo "\"dobi\" not found. Please install \"dobi\": https://github.com/dnephin/dobi/releases"
+            exit 1
+        fi
+    fi
+fi
+
+# check arguments
+if [[ -z ${1} || "list" == ${@: -1} ]]; then
+    ${dobi} --filename meta.yaml list
+    exit 1
+fi
+
+if [[ "version" !=  ${@: -1} ]]; then
+    ${dobi} --filename meta.yaml version # generate actual version files
+    # load generated version infos
+    source gen/gitversion/env/gitversion.env
+fi
 
 # execute dobi with meta as default
-exec dobi --filename meta.yaml ${@}
+exec ${dobi} --filename meta.yaml ${@}
